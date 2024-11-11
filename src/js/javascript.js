64ea0c93f92cb566522031a661135ab3a680382d
T2315 <nguyenthanhthuong.23052004@gmail.com>
2024-11-12 04:45:10 +0700

    Add and edit responsive for homepage

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const categoryLinks = document.querySelectorAll('.title-categories a');
     const containers = document.querySelectorAll('.container');
-  
+
     categoryLinks.forEach(link => {
       link.addEventListener('click', (e) => {
         e.preventDefault();
@@ -19,5 +19,35 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       });
     });
+
+    const typeSearch = document.getElementById('type-search');
+    const dropdownContent = document.querySelector('.dropdown-content');
+    const options = document.querySelectorAll('.dropdown-content a');
+
+    typeSearch.addEventListener('click', function(event) {
+        event.preventDefault();
+        dropdownContent.style.display = dropdownContent.style.display === 'none' ? 'block' : 'none';
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            options.forEach(opt => opt.classList.remove('active'));
+            option.classList.add('active');
+
+            const selectedIcon = option.getAttribute('data-icon');
+            typeSearch.innerHTML = `<i class="fa-solid ${selectedIcon} px-2"></i> <span>${option.textContent}</span><i class="fa-solid fa-caret-down px-2"></i>`;
+
+            dropdownContent.style.display = 'none';
+        });
+    });
+
+    document.addEventListener('click', function(event) {
+        if (!typeSearch.contains(event.target) && !dropdownContent.contains(event.target)) {
+            dropdownContent.style.display = 'none';
+        }
+    });
+    
   });
   
