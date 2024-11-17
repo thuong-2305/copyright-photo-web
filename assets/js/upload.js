e9707788944560c9398743afa9658b5689cfb627
T2315 <nguyenthanhthuong.23052004@gmail.com>
2024-11-18 00:11:06 +0700

    Complete basic feature upload image

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,51 +1,50 @@
-const info = document.querySelector(".upload")
-const overlay = document.querySelector(".overlay")
-const btnclose = document.querySelector(".close-user")
-const user = document.getElementById("btn-upload")
+const uploadButton = document.getElementById("btn-upload1");
+const overlay = document.querySelector(".overlay");
+const windowUpload = document.querySelector(".upload");
+const closeWindow = document.querySelector(".close-upload");
+const optionContent = document.querySelector(".option");
+const inputAdd = document.getElementById("file-upload");
+const paneShow = document.querySelector(".show-pane");
+const next = document.querySelector(".next");
+const contentTwo = document.querySelector(".content-2")
+const contentThree = document.querySelector(".content-3")
+const step1 = document.getElementById("step1");
+const step2 = document.getElementById("step2");
+const step3 = document.getElementById("step3");
+const navlist = document.querySelector(".nav-list");
+const succes = document.querySelector(".success");
 
-user.addEventListener('click', () => {
-    info.classList.add('active')
-    overlay.style.display = 'block'
+uploadButton.addEventListener('click', () => {
+    windowUpload.classList.add("active");
+    overlay.style.display = 'block';
 })
 
-btnclose.addEventListener('click', () => {
-    info.classList.remove('active')
-    overlay.style.display = 'none'
+closeWindow.addEventListener('click', () => {
+    windowUpload.classList.remove("active");
+    overlay.style.display = 'none';
+    paneShow.classList.remove("curr");
+    optionContent.classList.remove("pass");
 })
 
+inputAdd.addEventListener('click', (e) => {
+    e.preventDefault();
+    optionContent.classList.add("pass");
+    paneShow.classList.add("curr");
+})
 
+next.addEventListener('click', (e) => {
+    e.preventDefault();
+    step2.classList.add("complete");
+    step2.classList.remove("active");
+    step3.classList.add("active");
+    contentTwo.classList.add("curr");
+    navlist.classList.add("last");
+    contentThree.classList.add("curr");
+})
 
-const navItem = document.querySelectorAll(".nav-list li");
-const contentFrame = document.querySelector(".content-frame");
-const infoUser = document.querySelector(".info-user");
-const btnAgree = document.querySelector(".btn-agree");
-const navList = document.querySelector(".nav-list");
-
-function hideContent() {
-  contentFrame.style.display = "none";
-  infoUser.style.display = "block";
-  navList.classList.add("mid");
-  navItem[0].classList.remove("active");
-  navItem[0].classList.add("complete");
-  navItem[1].classList.add("active");
-}
-
-btnAgree.addEventListener("click", hideContent);
-
-const btnSave = document.querySelector(".btn-save");
-// const infoUser = document.querySelector(".info-user");
-const welcomeFrame = document.querySelector(".welcome-frame");
-
-function hideInfoUser(event) {
-  // Ngăn hành động mặc định của submit form quay vể đầu trang
-  event.preventDefault();
-
-  infoUser.style.display = "none";
-  welcomeFrame.style.display = "block";
-  navList.classList.add("last");
-  navItem[1].classList.remove("active");
-  navItem[1].classList.add("complete");
-  navItem[2].classList.add("active");
-}
-
-btnSave.addEventListener("click", hideInfoUser);
+succes.addEventListener('click', () => {
+    windowUpload.classList.remove("active");
+    overlay.style.display = 'none';
+    paneShow.classList.remove("curr");
+    optionContent.classList.remove("pass");
+})
