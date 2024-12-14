12de1f16defe4eea3e9a87b202dcebdfab89edfb
T2315 <nguyenthanhthuong.23052004@gmail.com>
2024-12-14 16:29:54 +0700

    tao cac doi tuong chung trong model

---
+++ src/main/java/vn/edu/hcmuaf/fit/coriphoto/model/Product.java
@@ -0,0 +1,121 @@
+package vn.edu.hcmuaf.fit.coriphoto.model;
+
+import java.time.LocalDate;
+
+public class Product {
+    private int id;
+    private int uid;
+    private int cid;
+    private String name;
+    private String description;
+    private String size;
+    private String dimension;
+    private LocalDate dateUpload;
+    private String url;
+    private double price;
+    private String status;
+
+    public Product() { }
+
+    public Product(int id, int uid, int cid, String name, String description, String size, String dimension, LocalDate dateUpload, String url, double price, String status) {
+        this.id = id;
+        this.uid = uid;
+        this.cid = cid;
+        this.name = name;
+        this.description = description;
+        this.size = size;
+        this.dimension = dimension;
+        this.dateUpload = dateUpload;
+        this.url = url;
+        this.price = price;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(String dimension) {
+        this.dimension = dimension;
+    }
+
+    public LocalDate getDateUpload() {
+        return dateUpload;
+    }
+
+    public void setDateUpload(LocalDate dateUpload) {
+        this.dateUpload = dateUpload;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/vn/edu/hcmuaf/fit/coriphoto/model/User.java
@@ -0,0 +1,81 @@
+package vn.edu.hcmuaf.fit.coriphoto.model;
+
+import java.time.LocalDate;
+
+public class User {
+    private int uid;
+    private int role;
+    private String fullName;
+    private String username;
+    private String password;
+    private String email;
+    private LocalDate createDate;
+
+    public User() {}
+
+    public User(int uid, int role, String fullName, String username, String password, String email, LocalDate createDate) {
+        this.uid = uid;
+        this.role = role;
+        this.fullName = fullName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.createDate = createDate;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDate createDate) {
+        this.createDate = createDate;
+    }
+}
