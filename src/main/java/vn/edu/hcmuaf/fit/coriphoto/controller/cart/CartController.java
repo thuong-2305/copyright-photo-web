12eaa99e75c43abf245a9d1a3558803272118e76
T2315 <nguyenthanhthuong.23052004@gmail.com>
2024-12-30 05:20:28 +0700

    Them cart

---
+++ src/main/java/vn/edu/hcmuaf/fit/coriphoto/controller/cart/CartController.java
@@ -0,0 +1,24 @@
+package vn.edu.hcmuaf.fit.coriphoto.controller.cart;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "CartController", value = "/cart")
+public class CartController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
