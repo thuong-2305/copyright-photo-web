581e783e8b2c4368a30675f2ab9af2cdff372c44
T2315 <nguyenthanhthuong.23052004@gmail.com>
2025-05-19 15:39:07 +0700

    Sua loi cast integer

--- src/main/java/vn/edu/hcmuaf/fit/coriphoto/dao/LogDAO.java
+++ src/main/java/vn/edu/hcmuaf/fit/coriphoto/dao/LogDAO.java
@@ -14,7 +14,7 @@ public class LogDAO {
 
     public void insertLog(ActivityLog log) {
         String sql = "INSERT INTO activity_log (severity, uid, username, eventDate, message) VALUES (:severity, :uid, :username, :eventDate, :message)";
-        jdbi.withHandle(handle -> handle.createUpdate(sql)
+        jdbi.withHandle(handle -> (Integer) handle.createUpdate(sql)
                 .bind("severity", log.getSeverity())
                 .bind("uid", log.getUid())
                 .bind("username", log.getUsername())
@@ -27,20 +27,5 @@ public class LogDAO {
         String sqlQuery = "SELECT * FROM activity_log";
         return jdbi.withHandle(handle -> handle.createQuery(sqlQuery)
                 .mapToBean(ActivityLog.class).list());
-    }
-
-    public static void main(String[] args) {
-        LogDAO logDAO = new LogDAO();
-        // Arrange
-        String severity = "INFO";
-        int uid = 1001;
-        String username = "john_doe";
-        LocalDateTime eventDate = LocalDateTime.now();
-        String message = "User logged in";
-
-        // Act
-        ActivityLog log = new ActivityLog(severity, uid, username, eventDate, message);
-
-        logDAO.insertLog(log);
     }
 }
